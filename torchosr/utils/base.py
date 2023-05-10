5967d5e386df777ca597860233c4904384ff43c8
Joana <asiakom24@vp.pl>
2023-05-10 17:20:59 +0200

    Comment

--- torchosr/utils/base.py
+++ torchosr/utils/base.py
@@ -4,15 +4,7 @@ from torchvision.transforms import Lambda
 
 def onehot_bg(n_classes):
     """
-    Convert evaluator scores to accumulative mean.
-
-    It's the best way to make reader capable to understand anything from your results.
-
-    :type scores: array-like, shape (n_estimators, n_chunks, n_metrics)
-    :param scores: Evaluation scores.
-
-    :rtype: array-like, shape (n_estimators, n_chunks, n_metrics)
-    :returns: Evaluation scores in format possible to read for human being.
+    One-hot encoder for classification labels
     """
     return Lambda(lambda y: torch.zeros(n_classes, dtype=torch.float).scatter_(0, y.clone().detach(), value=1))
 
