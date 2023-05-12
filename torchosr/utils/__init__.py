fab29e8f8fc427728e684bcd9cb91bf4f58912e2
Joana <asiakom24@vp.pl>
2023-05-12 18:04:26 +0200

    Include utils methods in init

--- torchosr/utils/__init__.py
+++ torchosr/utils/__init__.py
@@ -1,4 +1,4 @@
-from .base import (onehot_bg, inverse_transform, grayscale_transform)
+from .base import (onehot_bg, inverse_transform, grayscale_transform, get_openmax_epsilon, get_softmax_epsilon)
 
 __all__ = [
     "onehot_bg",
