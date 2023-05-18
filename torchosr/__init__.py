49ead7f67fa15d16dfa389c60f71d3d14b7a94ff
Joana <asiakom24@vp.pl>
2023-05-18 12:47:37 +0200

    Fix init

--- torchosr/__init__.py
+++ torchosr/__init__.py
@@ -1,10 +1,10 @@
-from . import models, data, utils
+from . import models, data, utils, architectures
 from ._version import __version__
 
 __all__ = [
     "models",
     "data",
     "utils",
-    "architectures"
+    "architectures",
     "__version__",
 ]
